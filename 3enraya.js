1f3c639b61df00777f443c4342be5348fc4ac63a
Jesus97 <jesusesma97@gmail.com>
2023-03-08 08:31:42 +0100

    segundo commit

--- 3enraya.js
+++ 3enraya.js
@@ -1,7 +1,13 @@
 
-var casilla1 = document.getElementsByClassName("boton1");
-casilla1.addEventListener("click", prueba); 
-   
-function prueba(){
-   console.log("prueba");
+var casillas = document.getElementsByClassName("boton");
+for(i=0;i<casillas.length;i++){
+   crearBotones(i)
 }
+
+function crearBotones(i){
+   casillas[i].addEventListener("click", function(event){prueba(i)});
+}
+function prueba(numero){
+   console.log("el numero es "+numero);
+}
+
